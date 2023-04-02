6462d52b98790036cf1eae75a9918ff6952f10d7
Riju Basu <whitedominator17@gmail.com>
2023-04-02 14:54:11 +0530

    about-me

--- src/Js/index.js
+++ src/Js/index.js
@@ -5,3 +5,20 @@ var typed = new Typed(" .typing", {
     backSpeed: 100,
     loop: true
 });
+
+
+let valueNumbers = document.querySelectorAll(".exp-number");
+let interval = 2000;
+
+valueNumbers.forEach((valueNumber) => {
+    let startValue = 0;
+    let endValue = parseInt(valueNumber.getAttribute("data-val"));
+    let duration = Math.floor(interval/endValue);
+    let counter = setInterval(() => {
+        startValue+=1;
+        valueNumber.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    },duration)
+});
