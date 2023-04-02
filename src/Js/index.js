8441bf5d580c5933d118b4b88e0cd625c0882dfd
Riju Basu <whitedominator17@gmail.com>
2023-04-02 18:00:16 +0530

    size

--- src/Js/index.js
+++ src/Js/index.js
@@ -1,24 +1,24 @@
+AOS.init();
 
 var typed = new Typed(" .typing", {
-    strings: ["Developer", "Writer", "AI Enthusiast"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true
+  strings: ["Developer", "Writer", "AI Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
 });
-
 
 let valueNumbers = document.querySelectorAll(".exp-number");
 let interval = 2000;
 
 valueNumbers.forEach((valueNumber) => {
-    let startValue = 0;
-    let endValue = parseInt(valueNumber.getAttribute("data-val"));
-    let duration = Math.floor(interval/endValue);
-    let counter = setInterval(() => {
-        startValue+=1;
-        valueNumber.textContent = startValue;
-        if (startValue == endValue) {
-            clearInterval(counter);
-        }
-    },duration)
+  let startValue = 0;
+  let endValue = parseInt(valueNumber.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(() => {
+    startValue += 1;
+    valueNumber.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
 });
